b86cc25952f47848c1eab528c0a58e08342f7ac1
Stephen Wiggins <steve.wiggins7@gmail.com>
2022-08-14 15:04:40 -0400

    saving works?

--- app/src/main/java/ca/avendor/pogostrings/MainActivity.kt
+++ app/src/main/java/ca/avendor/pogostrings/MainActivity.kt
@@ -22,38 +22,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        pogoStringList = ArrayList()
-        pogoStringList.add(PoGoString("Hello world"))
-        // method to load arraylist from shared prefs
-        // initializing our shared prefs with name as
-        // shared preferences.
-        val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
+        //pogoStringList = ArrayList()
+        //pogoStringList.add(PoGoString("Hello world"))
 
-        // creating a variable for gson.
-        val gson = Gson()
-
-        // below line is to get to string present from our
-        // shared prefs if not present setting it as null.
-        val json = sharedPreferences.getString("item", null)
-
-        // below line is to get the type of our array list.
-        val type: Type = object : TypeToken<ArrayList<PoGoString>>() {}.type
-
-        // in below line we are getting data from gson
-        // and saving it to our array list
-
-        //NOTE code stops here. May need another library or approach for getting the arraylist in and out of the storage
-
-        Log.d("arraylist", pogoStringList.toString())
-        //pogoStringList = gson.fromJson<Any>(json, type) as ArrayList<PoGoString>
-
-        // checking below if the array list is empty or not
-        if (pogoStringList == null) {
-            // if the array list is empty
-            // creating a new array list.
-
-
-        }
+        loadData()
         poGoStringAdapter = PoGoStringAdapter(pogoStringList)
 
         rvStringItems.adapter = poGoStringAdapter
@@ -87,10 +59,13 @@ class MainActivity : AppCompatActivity() {
             // and save data in shared prefs.
             editor.apply()
 
-            // after saving data we are displaying a toast message.
-            Toast.makeText(this, "Saved Array List to Shared preferences. ", Toast.LENGTH_SHORT)
-                .show()*/
+            // after saving data we are displaying a toast message.*/
+
             poGoStringAdapter.notifyDataSetChanged()
+            saveData()
+            Toast.makeText(this, "Saved Array List to Shared preferences. ", Toast.LENGTH_SHORT)
+                .show()
+
         }
     }
     private fun addItemToList(stringitem: String) {
@@ -99,5 +74,26 @@ class MainActivity : AppCompatActivity() {
 
         pogoStringList.add(PoGoString(stringitem))
 
+    }
+    private fun saveData() {
+        val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val gson = Gson()
+        val json = gson.toJson(pogoStringList)
+        editor.putString("task list", json)
+        editor.apply()
+    }
+
+    private fun loadData() {
+        val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
+        val gson = Gson()
+        val json = sharedPreferences.getString("task list", "")
+        val type = object: TypeToken<ArrayList<PoGoString>>() {
+        }.type
+
+        if(json == null)
+            pogoStringList = ArrayList()
+        else
+            pogoStringList = gson.fromJson(json, type)
     }
 }
