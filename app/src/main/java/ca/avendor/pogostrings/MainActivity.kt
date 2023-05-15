32a18a950c681ac6d25e382c6e0b89cc2ef51347
Stephen Wiggins <steve.wiggins7@gmail.com>
2023-05-14 20:38:30 -0400

    swipe to delete looks better

--- app/src/main/java/ca/avendor/pogostrings/MainActivity.kt
+++ app/src/main/java/ca/avendor/pogostrings/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,12 +16,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.DismissDirection
 import androidx.compose.material3.DismissValue
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -28,13 +33,17 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import ca.avendor.pogostrings.ui.theme.PoGoStringsTheme
 import com.google.gson.Gson
@@ -44,6 +53,9 @@ import com.google.gson.reflect.TypeToken
 class MainActivity : AppCompatActivity() {
 
     var pogoStringList: ArrayList<PoGoString> = ArrayList<PoGoString>()
+
+
+
     //private lateinit var poGoStringAdapter: PoGoStringAdapter
 
     //lateinit var rvStringItems: RecyclerView
@@ -70,17 +82,25 @@ class MainActivity : AppCompatActivity() {
     fun PoGoStringsApp(){
         val newString = remember { mutableStateOf(TextFieldValue()) }
 
+
         getStringList()
 
         Column{
             LazyColumn(Modifier.weight(1f)) {
-                items(pogoStringList.count()) { i ->
+                items(pogoStringList.size) { item ->
                     val dismissState = rememberDismissState()
-                    //TODO when this is swiped, remove the item from the list
-                    //TODO max width
-//https://daanidev.medium.com/swipe-to-delete-in-jetpack-compose-android-ca935a209c61
+                    if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                       // pogoStringList.removeAll(pogoStringList.filter { it.id == item })
+                        pogoStringList.removeAt(item)
+                        //save list
+                        saveData()
+                        getStringList()
+                    }
                     SwipeToDismiss(
                         state = dismissState,
+                        directions = setOf(
+                            DismissDirection.EndToStart
+                        ),
                         modifier = Modifier.padding(vertical = 1.dp),
                         background = {
                             val color by animateColorAsState(
@@ -89,7 +109,25 @@ class MainActivity : AppCompatActivity() {
                                     else -> Color.Red
                                 }, label = "blah"
                             )
-
+                            val scale by animateFloatAsState(
+                                if (dismissState.targetValue == DismissValue.Default) 0.75f else 1f,
+                                label = ""
+                            )
+                            val alignment = Alignment.CenterEnd
+                            val icon = Icons.Default.Delete
+                            Box(
+                                Modifier
+                                    .fillMaxSize()
+                                    .background(color)
+                                    .padding(horizontal = Dp(20f)),
+                                contentAlignment = alignment
+                            ) {
+                                Icon(
+                                    icon,
+                                    contentDescription = "Delete Icon",
+                                    modifier = Modifier.scale(scale)
+                                )
+                            }
                         },
                         dismissContent = {
                             Card (
@@ -103,7 +141,7 @@ class MainActivity : AppCompatActivity() {
                                         Modifier.fillMaxWidth(),
                                         horizontalArrangement = Arrangement.SpaceBetween,
                                     ){
-                                        Text(pogoStringList[i].item)
+                                        Text(pogoStringList[item].id.toString() + " " + pogoStringList[item].item)
                                         Button(
                                             onClick = {},
                                             Modifier.widthIn(100.dp),
@@ -136,10 +174,11 @@ class MainActivity : AppCompatActivity() {
                     label = { Text("New String") }
                 )
                 Button(onClick = {
-                    //Save the newString.Value to the list
-                    addItemToList(newString.value.text)
+                    addToStringList(newString.value.text)
+                    //save the list
+
                     println("Added new string")
-                    Modifier.widthIn(100.dp)
+
                 }) {
                     Text("Add New")
                 }
@@ -155,15 +194,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun addToStringList() {
-        val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        val gson = Gson()
-        val json = gson.toJson(pogoStringList)
-        editor.putString("strings", json)
-        editor.apply()
+    private fun addToStringList(stringitem: String) {
+
+        //val pogoStringList: ArrayList<PoGoString> = ArrayList<PoGoString>()
+       // val pogoStringList = getStringList()
+        if(pogoStringList.isEmpty()) {
+            pogoStringList.add(PoGoString(0, stringitem))
+        }else {
+            pogoStringList.add(PoGoString(pogoStringList.last().id + 1, stringitem))
+        }
+        saveData()
     }
     private fun getStringList(): ArrayList<PoGoString> {
+
+        //var pogoStringList: ArrayList<PoGoString> = ArrayList<PoGoString>()
         val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
         val gson = Gson()
         val json = sharedPreferences.getString("strings", "[]")
@@ -172,29 +216,24 @@ class MainActivity : AppCompatActivity() {
             pogoStringList = ArrayList()
         else
             pogoStringList = gson.fromJson(json, object : TypeToken<ArrayList<PoGoString>>() {}.type)
+
     return pogoStringList
 }
-    private fun addItemToList(stringitem: String) {
-        if(pogoStringList.isEmpty()) {
-            pogoStringList.add(PoGoString(0, stringitem))
-        }else {
-            pogoStringList.add(PoGoString(pogoStringList.last().id + 1, stringitem))
-        }
-        addToStringList()
-    }
+
         //pogoStringList.add(PoGoString(stringitem))
 
 
     //save to shared preferences. Converts to Json in order to save
-//    private fun saveData() {
-//        val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
-//        val editor = sharedPreferences.edit()
-//        val gson = Gson()
-//        val json = gson.toJson(pogoStringList)
-//        editor.putString("task list", json)
-//        editor.apply()
-//
-//    }
+    private fun saveData() {
+
+        val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val gson = Gson()
+        val json = gson.toJson(pogoStringList)
+        editor.putString("strings", json)
+        editor.apply()
+
+    }
 //
 //    private fun loadData() {
 //        val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
